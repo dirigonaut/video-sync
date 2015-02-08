08e056ea8169e5ae7071d14a928cab27de5d4569
slacker <calkser@gmail.com>
2015-02-07 17:10:58 -0800

    Prep for putting the UI together.  File browser now will select a file.  Need to pass this to the video streaming service and then point the video element to that video being streamed.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(window).load(function(){
+  function chooseFile(name) {
+    var chooser = $(name);
+    chooser.change(function(evt) {
+      console.log($(this).val());
+      $('#shared_video').attr({
+        "src": $(this).val(),
+        "autoplay": "autoplay",
+      });
+    });
+  }
+  chooseFile('#fileDialog');
+
+  videojs("shared_video", {}, function(){
+
+  });
+
+});
