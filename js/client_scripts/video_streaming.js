0e2837d7013fa529d603fe7203c9f41d07fd6a39
Sabo-san <dragonofshadows3726@gmail.com>
2015-08-01 11:49:24 -0400

    Audio works as it kind of always did...

--- js/client_scripts/video_streaming.js
+++ js/client_scripts/video_streaming.js
@@ -1,4 +1,5 @@
-var socket = io.connect('http://localhost:8080');
+
+var socket  = io.connect('http://localhost:8080');
 
 window.URL = window.URL || window.webkitURL;
 window.MediaSource = window.MediaSource || window.WebKitMediaSource;
@@ -6,52 +7,50 @@ window.MediaSource = window.MediaSource || window.WebKitMediaSource;
 var media_source;
 var buffer;
 
-var queue = [];
+var queue = new Array();
 var video = document.getElementById('shared_video');
-
-var end_of_stream = false;
 
 function video_streaming(){
   media_source = new MediaSource();
   video.src = window.URL.createObjectURL(media_source);
 
-  video.play();
+  media_source.addEventListener('sourceopen', media_source_callback, false);
+  media_source.addEventListener('webkitsourceopen', media_source_callback, false);
+  media_source.addEventListener('webkitsourceended', media_source_state , false);
+};
 
-  media_source.addEventListener('sourceopen', function(e) {
-    buffer = media_source.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
+function media_source_state(e){
+  console.log('media_source state: ' + e.readyState);
+};
 
-    buffer.addEventListener('error', function(e) { console.log('error: ' + media_source.readyState); });
-    buffer.addEventListener('abort', function(e) { console.log('abort: ' + media_source.readyState); });
+function media_source_callback(e) {
+  buffer = media_source.addSourceBuffer('video/webm; codecs="vp8,vorbis"');
 
-    buffer.addEventListener('update', function() {
-      if (queue.length > 0 && !buffer.updating) {
-        buffer.appendBuffer(queue.shift());
-      } else if(end_of_stream){
+  buffer.addEventListener('error', function(e) { console.log('error: ' + media_source.readyState); });
+  buffer.addEventListener('abort', function(e) { console.log('abort: ' + media_source.readyState); });
+
+  buffer.addEventListener('update', function(e) {
+    if (queue.length > 0 && !buffer.updating) {
+      buffer.appendBuffer(queue.shift());
+    } else {
+      media_source.endOfStream();
+    }
+  });
+
+  socket.emit('video-stream', '');
+};
+
+socket.on('video-packet', function(data){
+  console.log('video-packet');
+    if(data){
+      if (buffer.updating || queue.length > 0) {
+        queue.push(new Uint8Array(data));
+      } else {
+        buffer.appendBuffer(new Uint8Array(data));
+      }
+    } else {
+      if (!buffer.updating || queue.length == 0) {
         media_source.endOfStream();
       }
-    });
-
-    socket.emit('video-stream', "");
-  }, false);
-
-  media_source.addEventListener('sourceopen', function(e) { console.log('sourceopen: ' + media_source.readyState); });
-  media_source.addEventListener('sourceended', function(e) { console.log('sourceended: ' + media_source.readyState); });
-  media_source.addEventListener('sourceclose', function(e) { console.log('sourceclose: ' + media_source.readyState); });
-}
-
-socket.on("video-packet", function(data) {
-  if(data){
-    var chunk = new Uint8Array(data);
-    if (buffer.updating || queue.length > 0) {
-      queue.push(chunk);
-    } else {
-      buffer.appendBuffer(chunk);
     }
-  } else {
-    if (!buffer.updating || queue.length == 0) {
-      media_source.endOfStream();
-    } else {
-      end_of_stream = true;
-    }
-  }
 });
