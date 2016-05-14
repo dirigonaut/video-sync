c08f9d7e19bbe6f2084501a3fe0ff1ed6d842156
Sabo-kun <the.delirious.dirigonaut@gmail.com>
2016-05-13 20:19:29 -0700

    Stuff works again.

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,18 +1,20 @@
-var http 			         = require('http')
-var socketIO	         = require('socket.io');
+var Http        = require('http')
+var NodeStatic  = require('node-static');
+var SocketIO    = require('socket.io');
 
-var FileHandler        = require('./utils/FileHandler');
-var Bundler 		       = require('./utils/Bundler');
-var Validator		       = require('./utils/Validator');
-var Authenticator      = require('./authentication/Authenticator');
+var Bundler       = require('./utils/Bundler');
+var Validator     = require('./utils/Validator');
+var Authenticator = require('./authentication/Authenticator');
 
-var VideoController    = require('./video/VideoController');
-var StateController    = require('./state/StateController');
-var SmtpController     = require('./smtp/SmtpController');
-var DatabaseController = require('./database/DatabaseController');
+var VideoController     = require('./video/VideoController');
+var StateController     = require('./state/StateController');
+var SmtpController      = require('./smtp/SmtpController');
+var DatabaseController  = require('./database/DatabaseController');
 
-var app   = null;
-var io    = null;
+var app = null;
+var io  = null;
+var ns  = null;
+
 var admin = null;
 
 var validator;
@@ -20,11 +22,11 @@ var authenticator;
 
 function Server(callback) {
   if(app == null){
-    new Bundler();
+    ns  = new NodeStatic.Server('', {cache: 0, gzip: true});
+    app = new Http.createServer(handler);
+    io  = new SocketIO(app);
 
-    fileHandler     = new FileHandler();
-    app             = new http.createServer(fileHandler.handler());
-    io              = new socketIO(app);
+    new Bundler();
 
     validator		    = new Validator();
     authenticator   = new Authenticator();
@@ -38,18 +40,13 @@ function Server(callback) {
 
 module.exports = Server;
 
-function initialize () {
+function initialize() {
   io.on('connection', function (socket) {
     console.log("socket has connected: " + socket.id + " ip:" + socket.handshake.address);
     if(!socket.auth){
       if(socket.handshake.address == "::ffff:127.0.0.1" && admin == null){
         socket.auth = true;
         admin = socket;
-
-        new VideoController(io, socket);
-        new StateController(io, socket);
-        new SmtpController(io, socket);
-        new DatabaseController(io, socket);
       } else {
         socket.auth = false;
       }
@@ -60,6 +57,9 @@ function initialize () {
     new StateController(io, socket);
     new SmtpController(io, socket);
     new DatabaseController(io, socket);
+
+    //For client side callback
+    socket.emit('connected');
 
     socket.on('auth-user', function (data) {
       console.log('auth-user');
@@ -79,4 +79,11 @@ function initialize () {
       }
     }, 300000);
   });
+}
+
+function handler(request, response) {
+    console.log(request);
+    request.addListener('end', function() {
+      fileServer.serve(request, response);
+  }).resume();
 }
