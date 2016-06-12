c884f45d77698ee92f02c5cbfe52f0ee5a3798ce
dirigonaut <>
2016-06-12 14:57:02 -0700

    More gui clean up. Still need to hook up the session loading code to the ui.

---
+++ src/client/utils/FormDataSingleton.js
@@ -0,0 +1,22 @@
+var sessionList = [];
+var activeSession = null;
+
+var smtp = null;
+
+function FormDataSingleton() {
+
+};
+
+FormDataSingleton.prototype.initialize = function() {
+
+};
+
+FormDataSingleton.prototype.getSessionList = function() {
+
+};
+
+FormDataSingleton.prototype.getActiveSession = function() {
+
+};
+
+module.exports = FormDataSingleton;
