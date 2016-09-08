38a0bae92ed9ab0fe04f025725a65a4a1d33b250
dirigonaut <>
2016-09-07 21:25:02 -0700

    added some more tests for chat and debugged some of the flow.

--- static/resources/tests.js
+++ static/resources/tests.js
@@ -1,8 +1,16 @@
 function testSendChatCommand(command) {
-  var mesage = {"command" : command, "issuer" : client.getClientSocket().getSocketId(), "param" : null};
+  var message = {"command" : command, "issuer" : client.getClientSocket().getSocketId(), "param" : null};
   client.getClientSocket().sendRequest("chat-command", message);
 }
 
-function testSendChat(text) {
-  client.getClientSocket().sendRequest("chat-command", text);
+function testSendChat() {
+  client.getClientSocket().sendRequest("chat-broadcast", "broadcast");
 }
+
+function testSendPrivateChat(id) {
+  client.getClientSocket().sendRequest("chat-private", {"to": id, "text": "ping"});
+}
+
+client.getClientSocket().setEvent("chat-broadcast-resp", function(data) { console.log(data)});
+client.getClientSocket().setEvent("chat-ping-resp", function(data) { console.log(data)});
+client.getClientSocket().setEvent("chat-log-resp", function(data) { console.log(data)});
