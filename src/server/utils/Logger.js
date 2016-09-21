bbd4649d964b45d305af6450d857c669d2a5825a
Sabo-kun <the.delirious.dirigonaut@gmail.com>
2016-09-20 19:54:12 -0700

    Logging to chat now works.

--- src/server/utils/Logger.js
+++ src/server/utils/Logger.js
@@ -17,7 +17,7 @@ function Logger() {
 }
 
 Logger.prototype.trace = function(message) {
-  //log.trace(message);
+  log.trace(message);
   pingLog(Logger.Enum.TRACE, message);
 };
 
@@ -55,14 +55,21 @@ var setAdminSocket = function() {
 
   if(id != null) {
     var playerManager = new PlayerManager();
-    socket = playerManager.getPlayer(id);
+    var player = playerManager.getPlayer(id);
+
+    if(player !== null && player !== undefined) {
+      socket = player.socket;
+    }
   }
 };
 
 var pingLog = function(level, text) {
-  //if(level >= logLevel) {
-    console.log("here");
+  if(socket == null) {
+    setAdminSocket();
+  }
+
+  if(level >= logLevel) {
     var message = chatEngine.buildMessage(ChatEngine.SYSTEM, text);
     chatEngine.ping(ChatEngine.Enum.LOG, socket, message);
-  //}
+  }
 };
