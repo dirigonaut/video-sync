7748d1ca2548fe943c6d8a43693555eb158c531b
dirigonaut <>
2017-02-23 20:51:40 -0800

    The Cache is built out and is ready for testing.

--- src/server/utils/Cache.js
+++ src/server/utils/Cache.js
@@ -6,8 +6,14 @@ var LogManager  = require('../log/LogManager');
 
 var log = LogManager.getLog(LogManager.LogEnum.UTILS);
 
-var cacheMap = new Map();
+var cacheMap   = new Map();
+var heatMap    = new Map();
+
 var requestMap = new Map();
+
+var session    = new Session();
+
+const LIFE_TIME = 6000;
 
 var interval = setInterval(function() {
   pruneCache();
@@ -18,42 +24,76 @@ function Cache() {
 
 Cache.prototype.get = function(requestData, callback) {
   log.debug('get-segment');
+  var key = `${requestData.path}-${requestData.segment[0]}-${requestData.segment[1]}-${requestData.typeId}`;
 
-  if(!cacheMap.contains(requestData.path)) {
-    requestMap.set(requestData.path, [callback]);
-    cacheMap.set(requestData.path, undefined);
-
-    var readConfig = FileIO.createStreamConfig(session.getMediaPath() + requestData.path, function(data) {
-      var segment = new Object();
-      segment.typeId = typeId;
-      segment.data = data;
-
-      socket.emit("segment-chunk", segment);
-    });
-
-    if(requestData.segment) {
-      var options = {"start": parseInt(requestData.segment[0]), "end": parseInt(requestData.segment[1])};
-      readConfig.options = options;
-    } else {
-      log.debug("No segment options passed in.");
-    }
-
-    readConfig.onFinish = function() {
-      socket.emit("segment-end");
-    };
-
-    fileIO.read(readConfig);
+  if(!cacheMap.contains(key)) {
+    requestMap.set(key, [callback]);
+    readFile(key, requestData);
   } else {
-    if(cacheMap.get(requestData.path) !== undefined) {
-      callback(cacheMap.get(requestData.path));
+    if(cacheMap.get(key) !== undefined) {
+      callback(getCacheData(key));
     } else {
-      requestMap.get(requestData.path).push(callback);
+      requestMap.get(key).push(callback);
     }
   }
 };
 
 module.exports = Cache;
 
-function pruneCache() {
+function readFile(key, requestData) {
+  var fileIO = new FileIO();
 
+  var readConfig = fileIO.createStreamConfig(session.getMediaPath() + requestData.path, function(data) {
+    var segment = new Object();
+    segment.typeId = requestData.typeId;
+    segment.data = data;
+    handleCallbacks(key, segment);
+  });
+
+  var options = {"start": parseInt(requestData.segment[0]), "end": parseInt(requestData.segment[1])};
+  readConfig.options = options;
+
+  cacheMap.set(key, undefined);
+  fileIO.read(readConfig);
+}
+
+function pruneCache() {
+  var now = Date.now();
+
+  for(var pair of heatMap) {
+    var entry = pair[1];
+
+    var temp = [];
+    for(var i in entry) {
+      //maybe parse float
+      if(entry[i] - now < LIFE_TIME) {
+        entry.shift();
+      }
+    }
+
+    if(entry.length === 0) {
+      cacheMap.remove(pair[0]);
+    }
+  }
+}
+
+function getCacheData(key) {
+  var entry = heatMap.get(key);
+
+  if(entry) {
+    entry.push(Date.now());
+  } else {
+    entry = [Date.now()];
+    heatMap.set(key, entry);
+  }
+
+  return cacheMap.get(key);
+}
+
+function handleCallbacks(key, segment) {
+  for(var callback of requestMap.get(key)) {
+    callback(segment);
+  }
+
+  requestMap.remove(key);
 }
