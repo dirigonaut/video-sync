432fdad7f27a22289c8d7c61da8a4379cc830327
dirigonaut <>
2017-05-13 16:30:58 -0700

    Flushed out the config loader though EOL for paths still needs to be made cross os funcctional.

--- src/server/utils/Config.js
+++ src/server/utils/Config.js
@@ -1,14 +1,17 @@
-var appData = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + 'Library/Preferences' : process.env.HOME + '/.config')
-appData += '/video-sync/';
+var FileIO    = require("./FileIO");
+var FileUtils = require('./FileSystemUtils');
 
-var config;
+const APP_DATA = (process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + 'Library/Preferences' : process.env.HOME + '/.config')) + '/video-sync/';
+const CONFIG_NAME = "config.json"
+const DEFAULT_CONFIG = ;
+
+var config = null;
 
 function Config() {
+};
 
-}
-
-Config.prototype.initialize = function(path) {
-  config = loadConfig(appData);
+Config.prototype.initialize = function(callback) {
+  setupAppDataDir(callback);
 };
 
 Config.prototype.getConfig = function() {
@@ -16,15 +19,67 @@ Config.prototype.getConfig = function() {
 };
 
 Config.prototype.getAppDataDir = function() {
-  return appData;
+  return APP_DATA;
 };
 
 module.exports = Config;
 
-function setupAppDataDir(path) {
+function setupAppDataDir(callback) {
+  var fileIO = new FileIO();
+  var fileUtils = new FileUtils();
 
+  var checkAssets = function(exists) {
+    if(exists) {
+      var readStream = fileIO.createStreamConfig(APP_DATA, function(file) {
+        if(file !== undefined && file !== null) {
+          if(file === CONFIG_NAME) {
+            var setConfig = function(fileData) {
+              config = JSON.parse(fileData);
+              callback();
+            };
+
+            config = {};
+            loadConfig(`${APP_DATA}${CONFIG_NAME}`, setConfig);
+          }
+        } else if(file === null && config === null) {
+          createConfig(DEFAULT_CONFIG, `${APP_DATA}${CONFIG_NAME}`, callback);
+        }
+      });
+
+      fileIO.readDir(readStream, "json");
+    }
+  }
+
+  fileIO.ensureDirExists(APP_DATA, 484, checkAssets);
 };
 
-function loadConfig(path) {
-  return null;
+function loadConfig(path, callback) {
+  var fileIO = new FileIO();
+  var binaryData = [];
+
+  var readStream = fileIO.createStreamConfig(path, function(data) {
+    binaryData.push(data);
+  });
+
+  readStream.onFinish = function() {
+    callback(Buffer.concat(binaryData));
+  };
+
+  fileIO.read(readStream)
+};
+
+function createConfig(pathFrom, pathTo, callback) {
+  var saveToAppDir = function(fileData) {
+    var fileIO = new FileIO();
+
+    var writeStream = fileIO.createStreamConfig(pathTo, null);
+
+    writeStream.onFinish = function() {
+      callback();
+    };
+
+    fileIO.write(writeStream, fileData);
+  }
+
+  loadConfig(pathFrom, saveToAppDir);
 };
