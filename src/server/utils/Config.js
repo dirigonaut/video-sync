790a194abb57485b499e8f8c3ab249c5eeab648a
Sabo-kun <the.delirious.dirigonaut@gmail.com>
2017-05-28 12:35:25 -0700

    Cross OS configs added. And added the bluebird library for shifting to promises over callbacks.

--- src/server/utils/Config.js
+++ src/server/utils/Config.js
@@ -4,14 +4,19 @@ var Path      = require('path');
 var FileIO    = require("./FileIO");
 var FileUtils = require('./FileSystemUtils');
 
-const LOG_DIR  = "logs";
-const CONFIG_NAME = "config.json"
-const REDIS_CONFIG = "redis.conf"
+const CONFIG_NAME = "config.json";
+const CONFIG_NAME_OS = `config.${process.platform}.json`;
+
+const REDIS_CONFIG = "redis.conf";
+const REDIS_CONFIG_OS = `redis.${process.platform}.conf`;
+
 const ROOT_DIR = Path.join(__dirname, "../../../");
+const CONFIG_DIR = Path.join(ROOT_DIR, "configs");
 const APP_DATA = Path.join(process.env.APPDATA || Path.join(process.env.HOME,
   (process.platform == 'darwin' ?  Path.join('Library', 'Preferences') : '.config')), 'video-sync');
+const LOG_DIR  = Path.join(APP_DATA, "logs");
 
-var config = null;
+var config;
 
 class Config { }
 
@@ -41,7 +46,7 @@ Config.prototype.getAppDataDir = function() {
 };
 
 Config.prototype.getLogDir = function() {
-  return Path.join(APP_DATA, LOG_DIR);
+  return LOG_DIR;
 };
 
 module.exports = Config;
@@ -51,8 +56,8 @@ function setupAppDataDir(callback) {
 
   var checkAllAssets = function(exists) {
     if(exists) {
-      ensureAssetExists(REDIS_CONFIG, "conf")();
-      ensureAssetExists(CONFIG_NAME, "json", function(configuration) {
+      ensureAssetExists(REDIS_CONFIG, REDIS_CONFIG_OS, "conf")();
+      ensureAssetExists(CONFIG_NAME, CONFIG_NAME_OS, "json", function(configuration) {
         config = JSON.parse(configuration);
         callback();
       })();
@@ -60,7 +65,7 @@ function setupAppDataDir(callback) {
   };
 
   fileIO.ensureDirExists(APP_DATA, 484, checkAllAssets);
-  fileIO.ensureDirExists(Path.join(APP_DATA, LOG_DIR), 484, null);
+  fileIO.ensureDirExists(LOG_DIR, 484, null);
 };
 
 function loadConfig(path, callback) {
@@ -96,7 +101,7 @@ function createConfig(pathFrom, pathTo, callback) {
   loadConfig(pathFrom, saveToAppDir);
 };
 
-function ensureAssetExists(name, extType, callback) {
+function ensureAssetExists(name, pattern, extType, callback) {
   var assetFound = false;
 
   return function checkAssets() {
@@ -116,7 +121,8 @@ function ensureAssetExists(name, extType, callback) {
           }
         }
       } else if(file === null && !assetFound) {
-        createConfig(Path.join(ROOT_DIR, name), Path.join(APP_DATA, name), callback);
+        console.log('Creating ' + name);
+        createConfig(Path.join(CONFIG_DIR, pattern), Path.join(APP_DATA, name), callback);
       }
     });
 
