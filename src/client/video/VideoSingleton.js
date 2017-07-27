5ed182f8f82aca86acaf2cb6477b50e7326c770b
dirigonaut <>
2017-07-26 20:49:24 -0700

    More refactoring.

--- src/client/video/VideoSingleton.js
+++ src/client/video/VideoSingleton.js
@@ -1,92 +1,86 @@
 const Util   = require('util');
 const Events = require('events');
 
-var videoElement, metaData, socket, log;
+var videoElement, sourceEvents, metaData, socket, schemaFactory, log;
 
+function Video() { }
 
-function VideoSingleton() { }
+Video.prototype.initialize = function(force) {
+};
 
-VideoSingleton.prototype.initialize = function(mediaSource) {
+Video.prototype.setup = function() {
   var onTimeUpdateState = function() {
-    clientSocket.sendRequest('state-time-update', new RequestFactory().buildVideoStateRequest(videoElement));
+    socket.request('state-time-update', new RequestFactory().buildVideoStateRequest(videoElement));
   };
+
+  sourceEvents = [];
 
   videoElement.currentTime = 0;
   videoElement.addEventListener('timeupdate', onTimeUpdateState, false);
+  var activeMeta = metaManager.getActiveMetaData();
 
-  var videoEvents = [];
-  var audioEvents = [];
-  var activeMeta = _this.metaManager.getActiveMetaData();
-
-  if(activeMeta.active.get(SourceBuffer.Enum.VIDEO) !== undefined) {clientSocket
-    videoEvents = createVideoEvents(videoSingleton, SourceBuffer.Enum.VIDEO);
+  if(activeMeta.active.get(SourceBuffer.Enum.VIDEO)) {
+    sourceEvents[SourceBuffer.Enum.VIDEO] = createVideoEvents(this, SourceBuffer.Enum.VIDEO);
   }
 
-  if(activeMeta.active.get(SourceBuffer.Enum.AUDIO) !== undefined) {
-    audioEvents = createVideoEvents(videoSingleton, SourceBuffer.Enum.AUDIO);
+  if(activeMeta.active.get(SourceBuffer.Enum.AUDIO)) {
+    sourceEvents[SourceBuffer.Enum.AUDIO] = createVideoEvents(this, SourceBuffer.Enum.AUDIO);
   }
 
-  var resetVideo = function() {
+  this.emit('get-init');
+};
+
+Video.prototype.getVideoElement = function() {
+  return videoElement;
+};
+
+Video.prototype.setVideoElement = function(video) {
+  videoElement = video;
+};
+
+Video.prototype.onReset = function() {
+  return reset = function() {
     log.info(`Video reset.`);
     videoElement.removeEventListener('timeupdate', onTimeUpdateState, false);
 
-    if(videoEvents.length > 0) {
-      videoElement.removeEventListener('timeupdate', videoEvents[0], false);
-      videoElement.removeEventListener('seeking', videoEvents[1], false);
+    if(sourceEvents[SourceBuffer.Enum.VIDEO].length > 0) {
+      removeVideoSourceEvents(SourceBuffer.Enum.VIDEO);
     }
 
-    if(audioEvents.length > 0) {
-      videoElement.removeEventListener('timeupdate', audioEvents[0], false);
-      videoElement.removeEventListener('seeking', audioEvents[1], false);
+    if(sourceEvents[SourceBuffer.Enum.AUDIO].length > 0) {
+      removeVideoSourceEvents(SourceBuffer.Enum.AUDIO);
     }
-
-    mediaSource.removeEventListener('sourceended', resetVideo);
   };
+}
 
-  mediaSource.addEventListener('sourceended', resetVideo);
-};
-
-VideoSingleton.prototype.getVideoElement = function() {
-  return self.videoElement;
-};
-
-VideoSingleton.prototype.onProgress = function(typeId) {
+Video.prototype.onProgress = function(typeId) {
   var progress = function() {
     if(self.initialized) {
-      if(self.meta.isLastSegment(typeId, self.videoElement.currentTime)){
-        var timeToRequest = self.meta.isReadyForNextSegment(typeId, self.videoElement.currentTime);
-        if(timeToRequest !== null && timeToRequest === timeToRequest){
-          log.debug(`VideoSingleton.onProgress - time: ${timeToRequest} current: ${self.videoElement.currentTime}`);
-          self.emit("get-segment", typeId, timeToRequest);
+      if(meta.isLastSegment(typeId, videoElement.currentTime)){
+        var timeToRequest = meta.isReadyForNextSegment(typeId, videoElement.currentTime);
+        if(timeToRequest && timeToRequest === timeToRequest){
+          log.debug(`Video.onProgress - time: ${timeToRequest} current: ${videoElement.currentTime}`);
+          this.emit("get-segment", typeId, timeToRequest);
         }
       } else {
-        log.debug("VideoSingleton.onProgress - end of segments");
-        self.videoElement.removeEventListener('timeupdate', progress, false);
+        log.debug("Video.onProgress - end of segments");
+        videoElement.removeEventListener('timeupdate', progress, false);
       }
     }
-  };
+  }.bind(this);
 
   return progress;
 };
 
-VideoSingleton.prototype.onSeek = function(typeId) {
-  var seek = function() {
-    self.meta.updateActiveMeta(typeId, self.meta.getSegmentIndex(typeId, self.videoElement.currentTime));
-    self.emit("seek-segment", typeId, self.videoElement.currentTime);
-  };
-
-  return seek;
+Video.prototype.onSeek = function(typeId) {
+  return seek = function() {
+    meta.updateActiveMeta(typeId, meta.getSegmentIndex(typeId, videoElement.currentTime));
+    this.emit("seek-segment", typeId, videoElement.currentTime);
+  }.bind(this);
 };
 
-VideoSingleton.prototype.setup = function(callback) {
-  log.info("VideoSingleton.init");
-  self.initialized = true;
-  self.emit("get-init");
-  callback();
-};
-
-VideoSingleton.prototype.play = function() {
-  var video = this.videoElement;
+Video.prototype.play = function() {
+  var video = videoElement;
 
   if(video.readyState === 4) {
     log.debug("Set video to play");
@@ -97,31 +91,37 @@ VideoSingleton.prototype.play = function() {
   }
 };
 
-VideoSingleton.prototype.pause = function() {
+Video.prototype.pause = function() {
   var video = this.videoElement;
   self.videoElement.removeEventListener('canplay', resume, {"once": true});
   video.pause();
 };
 
-module.exports = VideoSingleton;
+module.exports = Video;
 
 function resume() {
-  var video = self.videoElement;
+  var video = videoElement;
   log.debug("Set video to play");
   video.play();
 }
 
-var createVideoEvents = function(videoSingleton, typeEnum) {
+var setVideoSourceEvents = function(typeEnum) {
   log.info(`Creating video events for buffer of type: ${typeEnum}.`);
   var bufferEvents = [];
 
-  var bufferUpdate = videoSingleton.onProgress(typeEnum);
+  var bufferUpdate = this.onProgress(typeEnum);
   videoElement.addEventListener('timeupdate', bufferUpdate, false);
   bufferEvents.push(bufferUpdate);
 
-  var bufferSeek = videoSingleton.onSeek(typeEnum);
+  var bufferSeek = this.onSeek(typeEnum);
   videoElement.addEventListener('seeking', bufferSeek, false);
   bufferEvents.push(bufferSeek);
 
   return bufferEvents;
+}
+
+var removeVideoSourceEvents = function(typeEnum) {
+  log.info(`Removing video events for buffer of type: ${typeEnum}.`);
+  videoElement.removeEventListener('timeupdate', sourceEvents[typeEnum][0], false);
+  videoElement.removeEventListener('seeking', sourceEvents[typeEnum][1], false);
 }
