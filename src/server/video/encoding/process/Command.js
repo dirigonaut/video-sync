7344657ffdd1bf09104bf4eed24cc4141db59e04
Sabo-kun_lp <danielcsabo@gmail.com>
2017-08-27 21:46:10 -0700

    A fix for two bugs dealing with the new EncodeFactory on the front end.

--- src/server/video/encoding/process/Command.js
+++ src/server/video/encoding/process/Command.js
@@ -1,72 +1,18 @@
-var fileSystemUtils, log;
+const REGEX = /-[A-Za-z:_-]*\s"[A-Za-z0-9=,\s]*"|-[A-Za-z:_-]*\s[^-][0-9A-Za-z-_"/\\.]*|-[A-Za-z:_-]*/g;
 
 function Command() { };
 
-Command.prototype.initialize = function(force) {
-  if(typeof Command.prototype.protoInit === 'undefined') {
-    Command.prototype.protoInit = true;
-    fileSystemUtils = this.factory.createFileSystemUtils();
-    var logManager  = this.factory.createLogManager();
-    log             = logManager.getLog(logManager.LogEnum.ENCODING);
+Command.prototype.parse = function(str) {
+  var regex = REGEX;
+  var args = [];
+  var lastIndex;
+
+  for(var itter = regex.exec(str); itter; itter = regex.exec(str)) {
+    lastIndex = regex.lastIndex;
+    args.push(itter[0]);
   }
+
+  return args.push(str.substring(lastIndex, str.length));
 };
 
-Command.prototype.parse = function(input) {
-  log.debug("Command.parse", input);
-  var args = [];
-  var options = input.split(" -");
-  var lastIndex = options.length-1;
-
-  var keyless = getTailKeylessPathValues(options[lastIndex]);
-  options[lastIndex] = options[lastIndex].substr(0, options[lastIndex].length - keyless.length);
-
-  for(var i = 0; i < options.length; ++i) {
-    args = args.concat(getKeyValuePair(options[i]));
-  }
-
-  if(keyless.length > 0) {
-    args = args.concat(keyless.split(" "));
-  }
-
-  log.debug("Parsed Arguments: ", args);
-  return args;
-}
-
 module.exports = Command;
-
-function getKeyValuePair(pair) {
-  log.debug("Command.getKeyValuePair", pair);
-  var keyValuePair = [];
-  var command = pair.split(" ");
-  var key = command.shift();
-  var value = "";
-
-  if(key !== null && key.trim() !== ""){
-    key = key.charAt(0) === "-" ? key : `-${key}`;
-    keyValuePair.push(key);
-
-    command.forEach(function(item){
-      value += value.length>1 ? " " + item : item;
-    });
-  }
-
-  if(value !== null && value.trim() !== "") {
-    keyValuePair.push(value);
-  }
-
-  return keyValuePair;
-}
-
-function getTailKeylessPathValues(command) {
-  log.debug("Command.getTailKeylessPathValues", command)
-  var keylessValues = "";
-  var parms = command.split(" ");
-
-  for(var x = 2; x < parms.length; ++x) {
-    if(fileSystemUtils.isPath(parms[x])) {
-      keylessValues += keylessValues == "" ? parms[x] : " " + parms[x];
-    }
-  }
-
-  return keylessValues;
-}
--- src/server/video/encoding/process/FfmpegProcess.js
+++ src/server/video/encoding/process/FfmpegProcess.js
@@ -3,7 +3,6 @@ const Events  = require('events');
 const Util    = require('util');
 
 const Spawn = require('child_process').spawn;
-const Split = require('split');
 
 var log;
 
@@ -30,8 +29,8 @@ FfmpegProcess.prototype.execute = function() {
   var ffmpeg = Spawn("ffmpeg", this.command);
   log.info(`Spawned child pid: ${ffmpeg.pid}`, this.command);
 
-  var processEvents = ['beforeExit', 'disconnect', 'exit', 'message'];
-  var streamEvents = ['close', 'data', 'end', 'error', 'readable'];
+  //var processEvents = ['beforeExit', 'disconnect', 'exit', 'message'];
+  //var streamEvents = ['close', 'data', 'end', 'error', 'readable'];
 
   ffmpeg.on('exit', function(data) {
     this.emit('exit', data);
@@ -41,12 +40,12 @@ FfmpegProcess.prototype.execute = function() {
   ffmpeg.stderr.on('data', function(data) {
     this.emit('error', data.toString('utf8'))
     console.log('ffmpeg.stderr.on(data): ' + data.toString('utf8'));
-  });
+  }.bind(this));
 
   ffmpeg.stdout.on('data', function(data) {
     this.emit('data', data.toString('utf8'));
     console.log('ffmpeg.stdout.on(data): ' + data.toString('utf8'));
-  });
+  }.bind(this));
 
   this.emit('start');
 
