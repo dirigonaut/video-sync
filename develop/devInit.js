961db7d8e00b122505ba35e912a01b2ae55fa927
dirigonaut <>
2017-09-10 10:20:24 -0700

    Adding documentation.

--- develop/devInit.js
+++ develop/devInit.js
@@ -1,10 +1,11 @@
-var Bundler = require('./Bundler');
+const Bundler = require('./Bundler');
+const path    = require('path');
+const marked  = require('marked');
+const fs      = require('fs');
+
 new Bundler();
 
-var marked = require('marked');
-var fs = require('fs');
-
-var readMe = fs.readFileSync('README.md', 'utf-8');
+var readMe = fs.readFileSync(path.join(__dirname, '../README.md'), 'utf-8');
 var markdownReadMe = marked(readMe);
 
-fs.writeFileSync('../static/html/info/README.html', markdownReadMe);
+fs.writeFileSync(path.join(__dirname,'../static/html/info/documentation.html'), markdownReadMe);
