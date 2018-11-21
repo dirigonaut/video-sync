d54ba9d0a02ed8e2628adc8a1d475ee21983d653
dirigonaut <>
2018-11-21 14:12:06 -0800

    Debugging

--- develop/shell/letsencrypt.sh
+++ develop/shell/letsencrypt.sh
@@ -2,18 +2,26 @@
 DOMAIN=$1
 DRY=$2
 
-sudo add-apt-repository ppa:certbot/certbot -y
-sudo apt-get install certbot software-properties-common -y
-sudo apt-get update
+add-apt-repository ppa:certbot/certbot -y
+apt-get install certbot software-properties-common -y
+apt-get update
 
-sudo certbot certonly --staging --standalone --agree-tos --preferred-challenges http --register-unsafely-without-email --renew-by-default -d $DOMAIN
+certbot certonly --staging --standalone --agree-tos --preferred-challenges http --register-unsafely-without-email --renew-by-default -d $DOMAIN
 
 if [[ ! -z $DRY ]]; then
   echo "Let's encrypt dry run."
 elif [[ $? == 0 ]]; then
   echo "Staging trial worked getting offical certs now."
-  sudo certbot certonly --standalone --agree-tos --preferred-challenges http --register-unsafely-without-email --renew-by-default -d $DOMAIN
-  sudo certbot renew --dry-run
+  certbot certonly --standalone --agree-tos --preferred-challenges http --register-unsafely-without-email --renew-by-default -d $DOMAIN
+  certbot renew --dry-run
+
+  echo "Symlink certs for Video-Sync"
+  CERT="$(sudo certbot certificates | grep 'Certificate Path:' | cut -d: -f2)"
+  KEY="$(sudo certbot certificates | grep 'Private Key Path:' | cut -d: -f2)"
+  rm /etc/video-sync/certificate.crt
+  rm /etc/video-sync/certificate.key
+  ln -s $CERT /etc/video-sync/certificate.crt
+  ln -s $KEY /etc/video-sync/certificate.key
 else
   echo "Let's  encrypt failed staging call"
   exit 1
