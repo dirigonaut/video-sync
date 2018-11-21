51a9d8a991afcefb76300021fe27974732882df9
sabo-san <dirigonaut>
2018-11-21 14:22:01 -0800

    Debugging

--- develop/shell/letsencrypt.sh
+++ develop/shell/letsencrypt.sh
@@ -9,20 +9,25 @@ apt-get update
 certbot certonly --staging --standalone --agree-tos --preferred-challenges http --register-unsafely-without-email --renew-by-default -d $DOMAIN
 RC=$?
 
-if [[ ! -z $DRY ]]; then
-  echo "Let's encrypt dry run."
-elif [[ $RC == 0 ]]; then
-  echo "Staging trial worked getting offical certs now."
-  certbot certonly --standalone --agree-tos --preferred-challenges http --register-unsafely-without-email --renew-by-default -d $DOMAIN
-  certbot renew --dry-run
+if [[ $RC == 0 ]]; then
+  if [[ -z $DRY ]]; then
+    echo "Staging trial worked getting offical certs now."
+    certbot certonly --standalone --agree-tos --preferred-challenges http --register-unsafely-without-email --renew-by-default -d $DOMAIN
+    certbot renew --dry-run
+  else
+    echo "Dry Run do not request certs"
+  fi
 
   echo "Symlink certs for Video-Sync"
   CERT="$(sudo certbot certificates | grep 'Certificate Path:' | cut -d: -f2)"
   KEY="$(sudo certbot certificates | grep 'Private Key Path:' | cut -d: -f2)"
-  rm /etc/video-sync/certificate.crt
-  rm /etc/video-sync/certificate.key
-  ln -s $CERT /etc/video-sync/certificate.crt
-  ln -s $KEY /etc/video-sync/certificate.key
+
+  if [[ ! -z $CERT ]]; then
+    rm /etc/video-sync/certificate.crt
+    rm /etc/video-sync/certificate.key
+    ln -s $CERT /etc/video-sync/certificate.crt
+    ln -s $KEY /etc/video-sync/certificate.key
+  fi
 else
   echo "Let's  encrypt failed staging call"
   exit 1
