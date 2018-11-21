793dd763eb14e16215265ec86988daef471e69ec
sabo-san <dirigonaut>
2018-11-21 12:11:46 -0800

    Debugging

--- develop/shell/letsencrypt.sh
+++ develop/shell/letsencrypt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DOMAIN=$1
-DRY=$2
+EMAIL=$2
+DRY=$3
 
 sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt-get install certbot software-properties-common -y
@@ -9,10 +10,10 @@ sudo apt-get update
 sudo certbot certonly --staging --standalone --renew-by-default -d $DOMAIN
 
 if [[ ! -z $DRY ]]; then
-  echo "Let's  encrypt dry run."
+  echo "Let's encrypt dry run."
 elif [[ $? == 0 ]]; then
   echo "Staging trial worked getting offical certs now."
-  sudo certbot certonly --standalone --renew-by-default -d $DOMAIN
+  sudo certbot certonly --standalone --preferred-challenges http --preferred-challenges http --renew-by-default -d $DOMAIN
   sudo certbot renew --dry-run
 else
   echo "Let's  encrypt failed staging call"
